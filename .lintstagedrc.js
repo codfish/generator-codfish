aeef29b43b8226a9ca485ddbeb5db5f51440226e
Chris O'Donnell <dev@codfish.io>
2019-09-16 23:40:48 -0400

    chore: migrate to cod-scripts

--- .lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{json,css,scss,html}': ['prettier --write', 'git add'],
-  '*.md': ['prettier --write', 'markdownlint -i **/templates/*.md', 'git add'],
-  '*.js': ['eslint --fix', 'git add'],
-};
